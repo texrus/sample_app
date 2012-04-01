c856c180923e8ba7980c1bae888f90270a7e1844
Slippy <daniel@texenglish.ru>
2012-04-01 20:12:48 +0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -62,6 +62,18 @@ describe "Authentication" do
           click_button "Sign in"
         end
 
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+
         describe "after signing in" do
 
           it "should render the desired protected page" do
@@ -125,6 +137,16 @@ describe "Authentication" do
 
         describe "visiting user index" do
           before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_selector('title', text: 'Sign in') }
         end
       end
